8a76ce57736db3e53f030a8855f9ebcdcbb46bc9
Akshay Pai <paiakshay998@gmail.com>
2019-08-03 10:17:31 +0530

    Add DAO methods

--- app/utils/web3ContractMethods.js
+++ app/utils/web3ContractMethods.js
@@ -1,5 +1,48 @@
-// import { DCodeInstance } from "./web3Conf";
+import { DCodeInstance } from "./web3Conf";
 
-// async function getOngoingContest() {
-//   return DCodeInstance
-// }
+getSendParams = async () => {
+  const accountIds = await web3.eth.getAccounts();
+  return {
+    from: accountIds[0],
+  };
+}
+
+async function getOngoingContest() {
+  return DCodeInstance.methods.getOngoingContest().call();
+}
+
+async function getContestDetails(contestId) {
+  return DCodeInstance.methods.getContestDetails(contestId).call();
+}
+
+async function addContest(contestName) {
+  return DCodeInstance.methods.addContest().send(getSendParams());
+}
+
+async function registerUser(contestId) {
+  return DCodeInstance.methods.registerUser(contestid).send(getSendParams());
+}
+
+async function addQuestion(contestId, problemIpfs, testcaseIpfs, hashAnswerIpfs) {
+  return DCodeInstance.methods.addQuestion(contestId, problemIpfs, testcaseIpfs, hashAnswerIpfs).send(getSendParams());
+}
+
+async function getCreatorContestIds() {
+  return DCodeInstance.methods.getCreatorContestIds().call();
+}
+
+async function getProblemDetails(contestId, problemIndex) {
+  return DCodeInstance.methods.getProblemDetails(contestId, problemIndex).call();
+}
+
+async function submitEntry(contestId, codeIpfs, problemIndex) {
+  return DCodeInstance.methods.submitEntry(contestId, codeIpfs, problemIndex).send(getSendParams());
+}
+
+async function getPendingSubmission() {
+  return DCodeInstance.methods.getPendingSubmission().call();
+}
+
+async function verifyResults(submissionId, isCorrect) {
+  return DCodeInstance.methods.verifyResults(submissionId, isCorrect).send(getSendParams());
+}
